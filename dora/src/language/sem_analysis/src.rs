dinfuehr/dora

lang: rename `struct` to `value`
Basic affordances are made such that `value`-named identifiers still work

fixes #73

dora/src/language/sem_analysis/src.rs
@@ -6,8 +6,8 @@ use dora_parser::ast;
 
 use crate::language::sem_analysis::{
     ClassDefinitionId, ConstDefinitionId, EnumDefinitionId, FctDefinitionId, FieldId,
-    GlobalDefinitionId, Intrinsic, StructDefinitionFieldId, StructDefinitionId, TraitDefinitionId,
-    TypeParamId,
+    GlobalDefinitionId, Intrinsic, TraitDefinitionId, TypeParamId, ValueDefinitionFieldId,
+    ValueDefinitionId,
 };
 use crate::language::ty::{SourceType, SourceTypeArray};
 
@@ -129,10 +129,10 @@ pub enum IdentType {
     Field(SourceType, FieldId),
 
     /// field expression: <expr>.<field_name>
-    StructField(SourceType, StructDefinitionFieldId),
+    ValueField(SourceType, ValueDefinitionFieldId),
 
     /// name of structure
-    Struct(StructDefinitionId),
+    Value(ValueDefinitionId),
 
     // name of constant
     Const(ConstDefinitionId),
@@ -155,9 +155,9 @@ impl IdentType {
         }
     }
 
-    pub fn struct_id(&self) -> StructDefinitionId {
+    pub fn struct_id(&self) -> ValueDefinitionId {
         match self {
-            &IdentType::Struct(sid) => sid,
+            &IdentType::Value(sid) => sid,
             _ => panic!(),
         }
     }
@@ -237,8 +237,8 @@ pub enum CallType {
     // Construct enum value
     Enum(SourceType, usize),
 
-    // Struct constructor call Struct(<args>)
-    Struct(StructDefinitionId, SourceTypeArray),
+    // Value constructor call Value(<args>)
+    Value(ValueDefinitionId, SourceTypeArray),
 
     // Used for *internal* functions (those are not exposed to Dora as Fct)
     Intrinsic(Intrinsic),
@@ -303,7 +303,7 @@ impl CallType {
             CallType::GenericStaticMethod(_, _, fctid) => Some(fctid),
             CallType::Intrinsic(_) => None,
             CallType::Enum(_, _) => None,
-            CallType::Struct(_, _) => None,
+            CallType::Value(_, _) => None,
             CallType::Lambda(_, _) => None,
             CallType::Class2Ctor(_, _) => None,
         }

dora/src/language/sem_analysis/values.rs
@@ -1,0 +1,234 @@
+use std::collections::hash_map::HashMap;
+use std::convert::TryInto;
+use std::sync::Arc;
+
+use dora_parser::ast;
+use dora_parser::interner::Name;
+use dora_parser::lexer::position::Position;
+
+use crate::language::sem_analysis::{
+    extension_matches, impl_matches, module_path, Candidate, ExtensionDefinitionId,
+    ImplDefinitionId, ModuleDefinitionId, PackageDefinitionId, SemAnalysis, SourceFileId,
+    TypeParamDefinition, TypeParamId, Visibility,
+};
+use crate::language::ty::{SourceType, SourceTypeArray};
+use crate::utils::Id;
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
+pub struct ValueDefinitionId(u32);
+
+impl ValueDefinitionId {
+    pub fn to_usize(self) -> usize {
+        self.0 as usize
+    }
+}
+
+impl Id for ValueDefinition {
+    type IdType = ValueDefinitionId;
+
+    fn id_to_usize(id: ValueDefinitionId) -> usize {
+        id.0 as usize
+    }
+
+    fn usize_to_id(value: usize) -> ValueDefinitionId {
+        ValueDefinitionId(value.try_into().unwrap())
+    }
+
+    fn store_id(value: &mut ValueDefinition, id: ValueDefinitionId) {
+        value.id = Some(id);
+    }
+}
+
+#[derive(Debug)]
+pub struct ValueDefinition {
+    pub id: Option<ValueDefinitionId>,
+    pub package_id: PackageDefinitionId,
+    pub module_id: ModuleDefinitionId,
+    pub file_id: SourceFileId,
+    pub ast: Arc<ast::Value>,
+    pub primitive_ty: Option<SourceType>,
+    pub type_params: Option<TypeParamDefinition>,
+    pub visibility: Visibility,
+    pub internal: bool,
+    pub internal_resolved: bool,
+    pub pos: Position,
+    pub name: Name,
+    pub fields: Vec<ValueDefinitionField>,
+    pub field_names: HashMap<Name, ValueDefinitionFieldId>,
+    pub impls: Vec<ImplDefinitionId>,
+    pub extensions: Vec<ExtensionDefinitionId>,
+}
+
+impl ValueDefinition {
+    pub fn new(
+        package_id: PackageDefinitionId,
+        module_id: ModuleDefinitionId,
+        file_id: SourceFileId,
+        node: &Arc<ast::Value>,
+    ) -> ValueDefinition {
+        ValueDefinition {
+            id: None,
+            package_id,
+            module_id,
+            file_id,
+            ast: node.clone(),
+            primitive_ty: None,
+            visibility: Visibility::from_ast(node.visibility),
+            pos: node.pos,
+            name: node.name,
+            internal: node.internal,
+            internal_resolved: false,
+            type_params: None,
+            fields: Vec::new(),
+            field_names: HashMap::new(),
+            impls: Vec::new(),
+            extensions: Vec::new(),
+        }
+    }
+
+    pub fn id(&self) -> ValueDefinitionId {
+        self.id.expect("missing id")
+    }
+
+    pub fn type_params(&self) -> &TypeParamDefinition {
+        self.type_params.as_ref().expect("uninitialized")
+    }
+
+    pub fn name(&self, sa: &SemAnalysis) -> String {
+        module_path(sa, self.module_id, self.name)
+    }
+
+    pub fn name_with_params(&self, sa: &SemAnalysis, type_params: &SourceTypeArray) -> String {
+        let mut name = self.name(sa);
+
+        if type_params.len() > 0 {
+            let type_params = type_params
+                .iter()
+                .map(|p| p.name(sa))
+                .collect::<Vec<_>>()
+                .join(", ");
+
+            name.push('[');
+            name.push_str(&type_params);
+            name.push(']');
+        }
+
+        name
+    }
+
+    pub fn ty(&self) -> SourceType {
+        if let Some(ref primitive_ty) = self.primitive_ty {
+            primitive_ty.clone()
+        } else {
+            let type_params = (0..self.type_params().len())
+                .into_iter()
+                .map(|id| SourceType::TypeParam(TypeParamId(id)))
+                .collect();
+            SourceType::Value(self.id(), SourceTypeArray::with(type_params))
+        }
+    }
+
+    pub fn all_fields_are_public(&self) -> bool {
+        // "Internal" structs don't have any outside visible fields.
+        if self.internal {
+            return false;
+        }
+
+        for field in &self.fields {
+            if !field.visibility.is_public() {
+                return false;
+            }
+        }
+
+        true
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub struct ValueDefinitionFieldId(pub usize);
+
+impl From<usize> for ValueDefinitionFieldId {
+    fn from(data: usize) -> ValueDefinitionFieldId {
+        ValueDefinitionFieldId(data)
+    }
+}
+
+impl ValueDefinitionFieldId {
+    pub fn to_usize(self) -> usize {
+        self.0
+    }
+}
+
+#[derive(Debug)]
+pub struct ValueDefinitionField {
+    pub id: ValueDefinitionFieldId,
+    pub pos: Position,
+    pub name: Name,
+    pub ty: SourceType,
+    pub visibility: Visibility,
+}
+
+pub fn find_methods_in_value(
+    sa: &SemAnalysis,
+    object_type: SourceType,
+    type_param_defs: &TypeParamDefinition,
+    name: Name,
+    is_static: bool,
+) -> Vec<Candidate> {
+    let struct_id = if object_type.is_primitive() {
+        object_type
+            .primitive_struct_id(sa)
+            .expect("primitive expected")
+    } else {
+        object_type.value_id().expect("struct expected")
+    };
+
+    let struct_ = sa.values.idx(struct_id);
+    let struct_ = struct_.read();
+
+    for &extension_id in &struct_.extensions {
+        if let Some(bindings) =
+            extension_matches(sa, object_type.clone(), type_param_defs, extension_id)
+        {
+            let extension = sa.extensions[extension_id].read();
+
+            let table = if is_static {
+                &extension.static_names
+            } else {
+                &extension.instance_names
+            };
+
+            if let Some(&fct_id) = table.get(&name) {
+                return vec![Candidate {
+                    object_type: object_type.clone(),
+                    container_type_params: bindings,
+                    fct_id,
+                }];
+            }
+        }
+    }
+
+    let mut candidates = Vec::new();
+
+    for &impl_id in &struct_.impls {
+        if let Some(bindings) = impl_matches(sa, object_type.clone(), type_param_defs, impl_id) {
+            let impl_ = sa.impls[impl_id].read();
+
+            let table = if is_static {
+                &impl_.static_names
+            } else {
+                &impl_.instance_names
+            };
+
+            if let Some(&method_id) = table.get(&name) {
+                candidates.push(Candidate {
+                    object_type: object_type.clone(),
+                    container_type_params: bindings.clone(),
+                    fct_id: method_id,
+                });
+            }
+        }
+    }
+
+    candidates
+}

dora/src/language/specialize.rs
@@ -61,7 +61,7 @@ pub fn replace_type_param(
             SourceType::Trait(trait_id, new_type_params)
         }
 
-        SourceType::Struct(struct_id, old_type_params) => {
+        SourceType::Value(struct_id, old_type_params) => {
             let new_type_params = SourceTypeArray::with(
                 old_type_params
                     .iter()
@@ -69,7 +69,7 @@ pub fn replace_type_param(
                     .collect::<Vec<_>>(),
             );
 
-            SourceType::Struct(struct_id, new_type_params)
+            SourceType::Value(struct_id, new_type_params)
         }
 
         SourceType::Enum(enum_id, old_type_params) => {

dora/src/language/typeparamck.rs
@@ -3,7 +3,7 @@ use dora_parser::lexer::position::Position;
 use crate::language::error::msg::ErrorMessage;
 use crate::language::sem_analysis::{
     implements_trait, ClassDefinitionId, EnumDefinitionId, FctDefinition, SemAnalysis,
-    SourceFileId, StructDefinitionId, TypeParamDefinition,
+    SourceFileId, TypeParamDefinition, ValueDefinitionId,
 };
 use crate::language::specialize::specialize_type;
 use crate::language::ty::{SourceType, SourceTypeArray};
@@ -33,20 +33,20 @@ pub fn check_enum(
     checker.check(type_params)
 }
 
-pub fn check_struct(
+pub fn check_value(
     sa: &SemAnalysis,
     fct: &FctDefinition,
-    struct_id: StructDefinitionId,
+    struct_id: ValueDefinitionId,
     type_params: &SourceTypeArray,
     error: ErrorReporting,
 ) -> bool {
-    let struct_ = sa.structs.idx(struct_id);
-    let struct_ = struct_.read();
+    let value = sa.values.idx(struct_id);
+    let value = value.read();
 
     let checker = TypeParamCheck {
         sa,
         caller_type_param_defs: &fct.type_params,
-        callee_type_param_defs: struct_.type_params(),
+        callee_type_param_defs: value.type_params(),
         error,
     };
 

dora/src/vm/known.rs
@@ -4,7 +4,7 @@ use crate::language::sem_analysis::{ClassDefinitionId, FctDefinitionId, TraitDef
 use crate::language::ty::{SourceType, SourceTypeArray};
 use crate::vm::{
     specialize_class_id, specialize_class_id_params, AnnotationDefinitionId, ClassInstanceId,
-    EnumDefinitionId, StructDefinitionId, VM,
+    EnumDefinitionId, ValueDefinitionId, VM,
 };
 
 #[derive(Debug)]
@@ -14,7 +14,7 @@ pub struct KnownElements {
     pub annotations: KnownAnnotations,
     pub functions: KnownFunctions,
     pub enums: KnownEnums,
-    pub structs: KnownStructs,
+    pub structs: KnownValue,
 
     pub byte_array_class_instance: Mutex<Option<ClassInstanceId>>,
     pub int_array_class_instance: Mutex<Option<ClassInstanceId>>,
@@ -36,7 +36,7 @@ impl KnownElements {
             traits: KnownTraits::new(),
             enums: KnownEnums::new(),
             annotations: KnownAnnotations::new(),
-            structs: KnownStructs::new(),
+            structs: KnownValue::new(),
 
             byte_array_class_instance: Mutex::new(None),
             int_array_class_instance: Mutex::new(None),
@@ -149,20 +149,20 @@ impl KnownClasses {
 }
 
 #[derive(Debug)]
-pub struct KnownStructs {
-    pub unit: Option<StructDefinitionId>,
-    pub bool: Option<StructDefinitionId>,
-    pub uint8: Option<StructDefinitionId>,
-    pub char: Option<StructDefinitionId>,
-    pub int32: Option<StructDefinitionId>,
-    pub int64: Option<StructDefinitionId>,
-    pub float32: Option<StructDefinitionId>,
-    pub float64: Option<StructDefinitionId>,
+pub struct KnownValue {
+    pub unit: Option<ValueDefinitionId>,
+    pub bool: Option<ValueDefinitionId>,
+    pub uint8: Option<ValueDefinitionId>,
+    pub char: Option<ValueDefinitionId>,
+    pub int32: Option<ValueDefinitionId>,
+    pub int64: Option<ValueDefinitionId>,
+    pub float32: Option<ValueDefinitionId>,
+    pub float64: Option<ValueDefinitionId>,
 }
 
-impl KnownStructs {
-    pub fn new() -> KnownStructs {
-        KnownStructs {
+impl KnownValue {
+    pub fn new() -> KnownValue {
+        KnownValue {
             unit: None,
             bool: None,
             uint8: None,
@@ -174,35 +174,35 @@ impl KnownStructs {
         }
     }
 
-    pub fn unit(&self) -> StructDefinitionId {
+    pub fn unit(&self) -> ValueDefinitionId {
         self.unit.expect("uninitialized")
     }
 
-    pub fn bool(&self) -> StructDefinitionId {
+    pub fn bool(&self) -> ValueDefinitionId {
         self.bool.expect("uninitialized")
     }
 
-    pub fn uint8(&self) -> StructDefinitionId {
+    pub fn uint8(&self) -> ValueDefinitionId {
         self.uint8.expect("uninitialized")
     }
 
-    pub fn char(&self) -> StructDefinitionId {
+    pub fn char(&self) -> ValueDefinitionId {
         self.char.expect("uninitialized")
     }
 
-    pub fn int32(&self) -> StructDefinitionId {
+    pub fn int32(&self) -> ValueDefinitionId {
         self.int32.expect("uninitialized")
     }
 
-    pub fn int64(&self) -> StructDefinitionId {
+    pub fn int64(&self) -> ValueDefinitionId {
         self.int64.expect("uninitialized")
     }
 
-    pub fn float32(&self) -> StructDefinitionId {
+    pub fn float32(&self) -> ValueDefinitionId {
         self.float32.expect("uninitialized")
     }
 
-    pub fn float64(&self) -> StructDefinitionId {
+    pub fn float64(&self) -> ValueDefinitionId {
         self.float64.expect("uninitialized")
     }
 }

core-lang/core

dora/src/language/sem_analysis/src.rs
@@ -6,8 +6,8 @@ use dora_parser::ast;
 
 use crate::language::sem_analysis::{
     ClassDefinitionId, ConstDefinitionId, EnumDefinitionId, FctDefinitionId, FieldId,
-    GlobalDefinitionId, Intrinsic, StructDefinitionFieldId, StructDefinitionId, TraitDefinitionId,
-    TypeParamId,
+    GlobalDefinitionId, Intrinsic, TraitDefinitionId, TypeParamId, ValueDefinitionFieldId,
+    ValueDefinitionId,
 };
 use crate::language::ty::{SourceType, SourceTypeArray};
 
@@ -129,10 +129,10 @@ pub enum IdentType {
     Field(SourceType, FieldId),
 
     /// field expression: <expr>.<field_name>
-    StructField(SourceType, StructDefinitionFieldId),
+    ValueField(SourceType, ValueDefinitionFieldId),
 
     /// name of structure
-    Struct(StructDefinitionId),
+    Value(ValueDefinitionId),
 
     // name of constant
     Const(ConstDefinitionId),
@@ -155,9 +155,9 @@ impl IdentType {
         }
     }
 
-    pub fn struct_id(&self) -> StructDefinitionId {
+    pub fn value_id(&self) -> ValueDefinitionId {
         match self {
-            &IdentType::Struct(sid) => sid,
+            &IdentType::Value(value_id) => value_id,
             _ => panic!(),
         }
     }
@@ -237,8 +237,8 @@ pub enum CallType {
     // Construct enum value
     Enum(SourceType, usize),
 
-    // Struct constructor call Struct(<args>)
-    Struct(StructDefinitionId, SourceTypeArray),
+    // Value constructor call Value(<args>)
+    Value(ValueDefinitionId, SourceTypeArray),
 
     // Used for *internal* functions (those are not exposed to Dora as Fct)
     Intrinsic(Intrinsic),
@@ -303,7 +303,7 @@ impl CallType {
             CallType::GenericStaticMethod(_, _, fctid) => Some(fctid),
             CallType::Intrinsic(_) => None,
             CallType::Enum(_, _) => None,
-            CallType::Struct(_, _) => None,
+            CallType::Value(_, _) => None,
             CallType::Lambda(_, _) => None,
             CallType::Class2Ctor(_, _) => None,
         }

dora/src/language/sem_analysis/values.rs
@@ -1,0 +1,234 @@
+use std::collections::hash_map::HashMap;
+use std::convert::TryInto;
+use std::sync::Arc;
+
+use dora_parser::ast;
+use dora_parser::interner::Name;
+use dora_parser::lexer::position::Position;
+
+use crate::language::sem_analysis::{
+    extension_matches, impl_matches, module_path, Candidate, ExtensionDefinitionId,
+    ImplDefinitionId, ModuleDefinitionId, PackageDefinitionId, SemAnalysis, SourceFileId,
+    TypeParamDefinition, TypeParamId, Visibility,
+};
+use crate::language::ty::{SourceType, SourceTypeArray};
+use crate::utils::Id;
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
+pub struct ValueDefinitionId(u32);
+
+impl ValueDefinitionId {
+    pub fn to_usize(self) -> usize {
+        self.0 as usize
+    }
+}
+
+impl Id for ValueDefinition {
+    type IdType = ValueDefinitionId;
+
+    fn id_to_usize(id: ValueDefinitionId) -> usize {
+        id.0 as usize
+    }
+
+    fn usize_to_id(value: usize) -> ValueDefinitionId {
+        ValueDefinitionId(value.try_into().unwrap())
+    }
+
+    fn store_id(value: &mut ValueDefinition, id: ValueDefinitionId) {
+        value.id = Some(id);
+    }
+}
+
+#[derive(Debug)]
+pub struct ValueDefinition {
+    pub id: Option<ValueDefinitionId>,
+    pub package_id: PackageDefinitionId,
+    pub module_id: ModuleDefinitionId,
+    pub file_id: SourceFileId,
+    pub ast: Arc<ast::Value>,
+    pub primitive_ty: Option<SourceType>,
+    pub type_params: Option<TypeParamDefinition>,
+    pub visibility: Visibility,
+    pub internal: bool,
+    pub internal_resolved: bool,
+    pub pos: Position,
+    pub name: Name,
+    pub fields: Vec<ValueDefinitionField>,
+    pub field_names: HashMap<Name, ValueDefinitionFieldId>,
+    pub impls: Vec<ImplDefinitionId>,
+    pub extensions: Vec<ExtensionDefinitionId>,
+}
+
+impl ValueDefinition {
+    pub fn new(
+        package_id: PackageDefinitionId,
+        module_id: ModuleDefinitionId,
+        file_id: SourceFileId,
+        node: &Arc<ast::Value>,
+    ) -> ValueDefinition {
+        ValueDefinition {
+            id: None,
+            package_id,
+            module_id,
+            file_id,
+            ast: node.clone(),
+            primitive_ty: None,
+            visibility: Visibility::from_ast(node.visibility),
+            pos: node.pos,
+            name: node.name,
+            internal: node.internal,
+            internal_resolved: false,
+            type_params: None,
+            fields: Vec::new(),
+            field_names: HashMap::new(),
+            impls: Vec::new(),
+            extensions: Vec::new(),
+        }
+    }
+
+    pub fn id(&self) -> ValueDefinitionId {
+        self.id.expect("missing id")
+    }
+
+    pub fn type_params(&self) -> &TypeParamDefinition {
+        self.type_params.as_ref().expect("uninitialized")
+    }
+
+    pub fn name(&self, sa: &SemAnalysis) -> String {
+        module_path(sa, self.module_id, self.name)
+    }
+
+    pub fn name_with_params(&self, sa: &SemAnalysis, type_params: &SourceTypeArray) -> String {
+        let mut name = self.name(sa);
+
+        if type_params.len() > 0 {
+            let type_params = type_params
+                .iter()
+                .map(|p| p.name(sa))
+                .collect::<Vec<_>>()
+                .join(", ");
+
+            name.push('[');
+            name.push_str(&type_params);
+            name.push(']');
+        }
+
+        name
+    }
+
+    pub fn ty(&self) -> SourceType {
+        if let Some(ref primitive_ty) = self.primitive_ty {
+            primitive_ty.clone()
+        } else {
+            let type_params = (0..self.type_params().len())
+                .into_iter()
+                .map(|id| SourceType::TypeParam(TypeParamId(id)))
+                .collect();
+            SourceType::Value(self.id(), SourceTypeArray::with(type_params))
+        }
+    }
+
+    pub fn all_fields_are_public(&self) -> bool {
+        // "Internal" structs don't have any outside visible fields.
+        if self.internal {
+            return false;
+        }
+
+        for field in &self.fields {
+            if !field.visibility.is_public() {
+                return false;
+            }
+        }
+
+        true
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub struct ValueDefinitionFieldId(pub usize);
+
+impl From<usize> for ValueDefinitionFieldId {
+    fn from(data: usize) -> ValueDefinitionFieldId {
+        ValueDefinitionFieldId(data)
+    }
+}
+
+impl ValueDefinitionFieldId {
+    pub fn to_usize(self) -> usize {
+        self.0
+    }
+}
+
+#[derive(Debug)]
+pub struct ValueDefinitionField {
+    pub id: ValueDefinitionFieldId,
+    pub pos: Position,
+    pub name: Name,
+    pub ty: SourceType,
+    pub visibility: Visibility,
+}
+
+pub fn find_methods_in_value(
+    sa: &SemAnalysis,
+    object_type: SourceType,
+    type_param_defs: &TypeParamDefinition,
+    name: Name,
+    is_static: bool,
+) -> Vec<Candidate> {
+    let value_id = if object_type.is_primitive() {
+        object_type
+            .primitive_value_id(sa)
+            .expect("primitive expected")
+    } else {
+        object_type.value_id().expect("struct expected")
+    };
+
+    let value = sa.values.idx(value_id);
+    let value = value.read();
+
+    for &extension_id in &value.extensions {
+        if let Some(bindings) =
+            extension_matches(sa, object_type.clone(), type_param_defs, extension_id)
+        {
+            let extension = sa.extensions[extension_id].read();
+
+            let table = if is_static {
+                &extension.static_names
+            } else {
+                &extension.instance_names
+            };
+
+            if let Some(&fct_id) = table.get(&name) {
+                return vec![Candidate {
+                    object_type: object_type.clone(),
+                    container_type_params: bindings,
+                    fct_id,
+                }];
+            }
+        }
+    }
+
+    let mut candidates = Vec::new();
+
+    for &impl_id in &value.impls {
+        if let Some(bindings) = impl_matches(sa, object_type.clone(), type_param_defs, impl_id) {
+            let impl_ = sa.impls[impl_id].read();
+
+            let table = if is_static {
+                &impl_.static_names
+            } else {
+                &impl_.instance_names
+            };
+
+            if let Some(&method_id) = table.get(&name) {
+                candidates.push(Candidate {
+                    object_type: object_type.clone(),
+                    container_type_params: bindings.clone(),
+                    fct_id: method_id,
+                });
+            }
+        }
+    }
+
+    candidates
+}

dora/src/language/specialize.rs
@@ -61,7 +61,7 @@ pub fn replace_type_param(
             SourceType::Trait(trait_id, new_type_params)
         }
 
-        SourceType::Struct(struct_id, old_type_params) => {
+        SourceType::Value(value_id, old_type_params) => {
             let new_type_params = SourceTypeArray::with(
                 old_type_params
                     .iter()
@@ -69,7 +69,7 @@ pub fn replace_type_param(
                     .collect::<Vec<_>>(),
             );
 
-            SourceType::Struct(struct_id, new_type_params)
+            SourceType::Value(value_id, new_type_params)
         }
 
         SourceType::Enum(enum_id, old_type_params) => {

dora/src/language/typeparamck.rs
@@ -3,7 +3,7 @@ use dora_parser::lexer::position::Position;
 use crate::language::error::msg::ErrorMessage;
 use crate::language::sem_analysis::{
     implements_trait, ClassDefinitionId, EnumDefinitionId, FctDefinition, SemAnalysis,
-    SourceFileId, StructDefinitionId, TypeParamDefinition,
+    SourceFileId, TypeParamDefinition, ValueDefinitionId,
 };
 use crate::language::specialize::specialize_type;
 use crate::language::ty::{SourceType, SourceTypeArray};
@@ -33,20 +33,20 @@ pub fn check_enum(
     checker.check(type_params)
 }
 
-pub fn check_struct(
+pub fn check_value(
     sa: &SemAnalysis,
     fct: &FctDefinition,
-    struct_id: StructDefinitionId,
+    value_id: ValueDefinitionId,
     type_params: &SourceTypeArray,
     error: ErrorReporting,
 ) -> bool {
-    let struct_ = sa.structs.idx(struct_id);
-    let struct_ = struct_.read();
+    let value = sa.values.idx(value_id);
+    let value = value.read();
 
     let checker = TypeParamCheck {
         sa,
         caller_type_param_defs: &fct.type_params,
-        callee_type_param_defs: struct_.type_params(),
+        callee_type_param_defs: value.type_params(),
         error,
     };
 

dora/src/vm/known.rs
@@ -4,7 +4,7 @@ use crate::language::sem_analysis::{ClassDefinitionId, FctDefinitionId, TraitDef
 use crate::language::ty::{SourceType, SourceTypeArray};
 use crate::vm::{
     specialize_class_id, specialize_class_id_params, AnnotationDefinitionId, ClassInstanceId,
-    EnumDefinitionId, StructDefinitionId, VM,
+    EnumDefinitionId, ValueDefinitionId, VM,
 };
 
 #[derive(Debug)]
@@ -14,7 +14,7 @@ pub struct KnownElements {
     pub annotations: KnownAnnotations,
     pub functions: KnownFunctions,
     pub enums: KnownEnums,
-    pub structs: KnownStructs,
+    pub values: KnownValue,
 
     pub byte_array_class_instance: Mutex<Option<ClassInstanceId>>,
     pub int_array_class_instance: Mutex<Option<ClassInstanceId>>,
@@ -36,7 +36,7 @@ impl KnownElements {
             traits: KnownTraits::new(),
             enums: KnownEnums::new(),
             annotations: KnownAnnotations::new(),
-            structs: KnownStructs::new(),
+            values: KnownValue::new(),
 
             byte_array_class_instance: Mutex::new(None),
             int_array_class_instance: Mutex::new(None),
@@ -149,20 +149,20 @@ impl KnownClasses {
 }
 
 #[derive(Debug)]
-pub struct KnownStructs {
-    pub unit: Option<StructDefinitionId>,
-    pub bool: Option<StructDefinitionId>,
-    pub uint8: Option<StructDefinitionId>,
-    pub char: Option<StructDefinitionId>,
-    pub int32: Option<StructDefinitionId>,
-    pub int64: Option<StructDefinitionId>,
-    pub float32: Option<StructDefinitionId>,
-    pub float64: Option<StructDefinitionId>,
+pub struct KnownValue {
+    pub unit: Option<ValueDefinitionId>,
+    pub bool: Option<ValueDefinitionId>,
+    pub uint8: Option<ValueDefinitionId>,
+    pub char: Option<ValueDefinitionId>,
+    pub int32: Option<ValueDefinitionId>,
+    pub int64: Option<ValueDefinitionId>,
+    pub float32: Option<ValueDefinitionId>,
+    pub float64: Option<ValueDefinitionId>,
 }
 
-impl KnownStructs {
-    pub fn new() -> KnownStructs {
-        KnownStructs {
+impl KnownValue {
+    pub fn new() -> KnownValue {
+        KnownValue {
             unit: None,
             bool: None,
             uint8: None,
@@ -174,35 +174,35 @@ impl KnownStructs {
         }
     }
 
-    pub fn unit(&self) -> StructDefinitionId {
+    pub fn unit(&self) -> ValueDefinitionId {
         self.unit.expect("uninitialized")
     }
 
-    pub fn bool(&self) -> StructDefinitionId {
+    pub fn bool(&self) -> ValueDefinitionId {
         self.bool.expect("uninitialized")
     }
 
-    pub fn uint8(&self) -> StructDefinitionId {
+    pub fn uint8(&self) -> ValueDefinitionId {
         self.uint8.expect("uninitialized")
     }
 
-    pub fn char(&self) -> StructDefinitionId {
+    pub fn char(&self) -> ValueDefinitionId {
         self.char.expect("uninitialized")
     }
 
-    pub fn int32(&self) -> StructDefinitionId {
+    pub fn int32(&self) -> ValueDefinitionId {
         self.int32.expect("uninitialized")
     }
 
-    pub fn int64(&self) -> StructDefinitionId {
+    pub fn int64(&self) -> ValueDefinitionId {
         self.int64.expect("uninitialized")
     }
 
-    pub fn float32(&self) -> StructDefinitionId {
+    pub fn float32(&self) -> ValueDefinitionId {
         self.float32.expect("uninitialized")
     }
 
-    pub fn float64(&self) -> StructDefinitionId {
+    pub fn float64(&self) -> ValueDefinitionId {
         self.float64.expect("uninitialized")
     }
 }